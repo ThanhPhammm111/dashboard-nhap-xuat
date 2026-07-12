68157956e72481031dde43fa84c7d54beaf2c6e8
Thanh Pham <thanhpham0654@gmail.com>
2026-07-12 16:46:00 +0700

    Move batch files to Script folder and clean up root directory

---
+++ Script/KichHoatTuDong.bat
@@ -0,0 +1,5 @@
+@echo off
+title He Thong Doi Soat Tu Dong - Antigravity
+echo Dang khoi dong he thong theo doi tu dong...
+powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0AutoMonitor.ps1"
+pause
